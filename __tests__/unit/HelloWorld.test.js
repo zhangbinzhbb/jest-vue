c17a3ad90266e73cc10c4bba07ff46a81f9ecbfa
张标斌 <13020106399@163.com>
2020-07-16 14:55:24 +0800

    优化jest config 配置

--- __tests__/unit/HelloWorld.test.js
+++ __tests__/unit/HelloWorld.test.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
+    console.log('123a')
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
